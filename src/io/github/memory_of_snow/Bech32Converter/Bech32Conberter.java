77a7a1cc9683a70f3e7f0bfae53eafbcd3b46770
memory-of-snow <memory.o.f.snow.main@gmail.com>
2023-05-26 17:30:49 +0900

    許される配列のキャラクター配列導入など

--- src/io/github/memory_of_snow/Bech32Converter/Bech32Conberter.java
+++ src/io/github/memory_of_snow/Bech32Converter/Bech32Conberter.java
@@ -5,14 +5,19 @@ import java.util.logging.Logger;
 
 public class Bech32Conberter {
 
-    static private Logger log = Logger.getLogger("Bech32Conberter");
+    static private final Logger log = Logger.getLogger("Bech32Conberter");
     static private final String publicKeyBech32String = "npub17x6pn22ukq3n5yw5x9prksdyyu6ww9jle2ckpqwdprh3ey8qhe6stnpujh";
 
     //Bech32における人間の可読部(HRP:human-readable part)とデータ部分(data part)の区切り文字(separator)、常に"1"
-    static private final String HRP_NPUB_LOWER_CASE = "npub";
-    static private final String HRP_NSEC_LOWER_CASE = "nsec";
-    static private final String HRP_NOTE_LOWER_CASE = "note";
-    static public String BECH32SEPARETOR = "1";
+
+    // "1bio"は使用しない
+    static private final char[] AllowCharacters = {
+            '0','2','3','4','5','6','7','8','9',
+            'a','c','d','e','f','g','h','i','j','k','m','n','p','q','r','s','t','u','v','w','x','y','z',
+
+            //仮置き、2023.05.26時点では大文字を許容する
+            'A','C','D','E','F','G','H','I','J','K','M','N','P','Q','R','S','T','U','V','W','X','Y','Z',
+    };
 
     public String ConvertBech32toHex (String inputString){
 
@@ -28,7 +33,7 @@ public class Bech32Conberter {
         if(inputString == null){
             log.log(Level.FINE,"input is null.");
             return false;
-        };
+        }
 
         //許容される最大は90文字
         if(inputString.length() > 90){
@@ -42,13 +47,12 @@ public class Bech32Conberter {
         String separator = inputString.substring(4,5);
         String dataPart = inputString.substring(5);
 
-        if(!hrp.equals(HRP_NOTE_LOWER_CASE) && !hrp.equals(HRP_NPUB_LOWER_CASE) && !hrp.equals(HRP_NSEC_LOWER_CASE)
-                && !hrp.equals(HRP_NOTE_LOWER_CASE.toUpperCase()) && !hrp.equals(HRP_NPUB_LOWER_CASE.toUpperCase()) && !hrp.equals(HRP_NSEC_LOWER_CASE.toUpperCase())){
+        if(!isAllowHumanReadablePart(hrp)){
             log.log(Level.FINE,"hrp is wrong.");
             return false;
         }
 
-        if(!separator.equals(BECH32SEPARETOR)){
+        if(!separator.equals("1")){
             log.log(Level.FINE,"separator is wrong.");
             return false;
         }
@@ -91,6 +95,11 @@ public class Bech32Conberter {
         }
 
         return true;
+    }
+
+    private static boolean isAllowHumanReadablePart(String hrp) {
+        return hrp.equals("npub") || hrp.equals("nsec") || hrp.equals("note")
+                || hrp.equals("NPUB") || hrp.equals("NSEC") || hrp.equals("NOTE");
     }
 
 }
--- src/io/github/memory_of_snow/Bech32Converter/MinimalUse.java
+++ src/io/github/memory_of_snow/Bech32Converter/MinimalUse.java
@@ -1,10 +1,62 @@
 package io.github.memory_of_snow.Bech32Converter;
 
+import org.apache.commons.codec.binary.Hex;
+import org.bitcoinj.core.Bech32;
+
 public class MinimalUse {
 
     public static void main(String[] args){
 
-        return;
+        operationConfirmation();
 
+    }
+
+    private static void operationConfirmation() {
+        StringBuilder outputStringBuiler = new StringBuilder();
+
+        final byte[] CHARSET_REV = {
+                -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+                -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+                -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+                15, -1, 10, 17, 21, 20, 26, 30, 7, 5, -1, -1, -1, -1, -1, -1,
+                -1, 29, -1, 24, 13, 25, 9, 8, 23, -1, 18, 22, 31, 27, 19, -1,
+                1, 0, 3, 16, 11, 28, 12, 14, 6, 4, 2, -1, -1, -1, -1, -1,
+                -1, 29, -1, 24, 13, 25, 9, 8, 23, -1, 18, 22, 31, 27, 19, -1,
+                1, 0, 3, 16, 11, 28, 12, 14, 6, 4, 2, -1, -1, -1, -1, -1
+        };
+
+        int boxsize = CHARSET_REV.length;
+
+        byte[] ContrastTable = new byte[boxsize];
+
+        byte target = 0;
+        for(int i = 0; i < boxsize ; i++){
+            ContrastTable[i]=target;
+            target++;
+        }
+
+
+        for(int i = 0 ; i < boxsize ; i++ ){
+            outputStringBuiler.append("index:").append(i).append(" ");
+            outputStringBuiler.append("byte:").append(ContrastTable[i]).append(" ");
+            outputStringBuiler.append("char:").append(String.valueOf((char)ContrastTable[i])).append(" ");
+            outputStringBuiler.append("target:").append(CHARSET_REV[i]).append("\n");
+        }
+
+        System.out.println(outputStringBuiler);
+    }
+
+    private static void convertBech32ToBech32Data() {
+        String publicKey = "npub17x6pn22ukq3n5yw5x9prksdyyu6ww9jle2ckpqwdprh3ey8qhe6stnpujh";
+
+        System.out.println("input publicKey = " + publicKey );
+
+        Bech32.Bech32Data decordData = Bech32.decode(publicKey);
+
+        String hrp = decordData.hrp;
+
+        System.out.println("hrp = " + hrp);
+        System.out.println("encord = " + decordData.encoding);
+        System.out.println("dataPart = " + new String(Hex.encodeHex(decordData.data)));
     }
 }
